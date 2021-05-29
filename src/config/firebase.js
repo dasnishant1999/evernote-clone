140a5267c0530f6acc11a320c7592046217de6a0
dasnishant1999 <dasnishant1999@gmail.com>
2021-05-29 19:29:59 +0530

    updated firebase config keys

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyANcaS-h1lhTMVhvs1l9hlXBXdKbmMp2Qs",
+  authDomain: "evernote-clone-4fb91.firebaseapp.com",
+  projectId: "evernote-clone-4fb91",
+  storageBucket: "evernote-clone-4fb91.appspot.com",
+  messagingSenderId: "345751373222",
+  appId: "1:345751373222:web:b61e577cdb67003932fb67",
+  measurementId: "G-6B4WTBFQ4G",
 });
 
 const db = firebase.firestore();
